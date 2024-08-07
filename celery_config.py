5d48738f0fe246e0411f3838f8597b6d80b1c2df
michaelhurtado4 <143548938+michaelhurtado4@users.noreply.github.com>
2024-08-07 18:14:05 -0500

    Update celery_config.py

--- celery_config.py
+++ celery_config.py
@@ -4,26 +4,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-celery_app = Celery('tasks', broker=os.environ.get("redis_url"), backend=os.environ.get("redis_url"))
-
-celery_app.conf.beat_schedule = {
-    'run-get-likedsongs-every-30-seconds': {
-        'task': 'tasks.get_likedsongs',
-        'schedule': 30.0,  # Every 30 seconds
-    },
-}
-
-celery_app.autodiscover_tasks(['tasks'])  # Adjust with the actual name of your tasks module if necessary
-
-# Task routing
-celery_app.conf.task_routes = {
-    'tasks.get_likedsongs': {'queue': 'cloud_queue'},  # Route 'tasks.get_likedsongs' tasks to 'cloud_queue'
-}
+celery_app = Celery('tasks', broker=os.environ.get('redis_url'), backend=os.environ.get('redis_url'))
 
 celery_app.conf.update(
-    result_expires=60,  # 1 minute
+    result_expires=60,
+    task_routes={
+        'tasks.get_likedsongs': {'queue': 'cloud_queue'},
+    },
+    beat_schedule={
+        'run-get-likedsongs-every-30-seconds': {
+            'task': 'tasks.get_likedsongs',
+            'schedule': 30.0,
+        },
+    },
 )
 
+celery_app.autodiscover_tasks(['tasks'])
 import tasks  # Import tasks module to register tasks
 
 # Check Redis connection
